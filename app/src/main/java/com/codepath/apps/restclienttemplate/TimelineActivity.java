98b3f9b732223719509cef1335952f7a503bbe11
dhrutip <dhruti.pandya@gmail.com>
2021-06-28 17:13:19 -0700

    created menu icon for composing tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,8 +6,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -78,9 +81,26 @@ public class TimelineActivity extends AppCompatActivity {
         });
     }
 
-
     private void onLogoutButton() {
         client.clearAccessToken(); // forget who's logged in
         finish(); // navigate backwards to Login screen
+    }
+
+    // Inflate the menu; this adds items to the action bar if it is present.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.compose) {
+            // compose icon is tapped
+            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
+            return true;
+            // navigate to compose activity
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

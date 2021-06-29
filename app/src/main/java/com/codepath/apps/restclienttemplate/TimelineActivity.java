14379151129807375d569dc0ac120504dd639ceb
dhrutip <dhruti.pandya@gmail.com>
2021-06-29 14:10:25 -0700

    compose tweet screen setup

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -97,9 +98,11 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.compose) {
             // compose icon is tapped
-            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
-            return true;
+            // Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
             // navigate to compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
